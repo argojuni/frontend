d28f5ca4e81b6916226009e672b5eead445c0ee3
argojuni <juniargoprasaja05@gmail.com>
2024-12-21 14:26:19 +0700

    belajar-tailwind-1

---
+++ Tailwind-CSS/Tailwind1/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npx tailwind -o ./public/css/final.css --minify
